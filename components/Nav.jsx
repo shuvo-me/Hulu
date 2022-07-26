e406ba03392d325c838caa5d50992baa455e1a8c
shuvo-me <shuvonaim123@gmail.com>
2022-07-26 08:18:03 +0600

    component naming

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,18 +1,26 @@
-import requests from "@/assets/json/requests"
-import { useRouter } from "next/router"
+import requests from "@/assets/json/requests";
+import { useRouter } from "next/router";
 
 const Nav = () => {
-    const router = useRouter()
+  const router = useRouter();
   return (
     <nav className="relative">
-        <div className="flex px-10 sm:px-20 whitespace-nowrap space-x-10 sm:space-x-20 overflow-x-scroll text-2xl scrollbar-hide">
-        {Object.entries(requests).map(([key,{title,url}])=>(
-           <h2 onClick={()=> router.push(`/?genere=${key}`)} key={Math.random()} className=" last:pr-24 cursor-pointer transition duration-100 transform hover:scale-125 hover:text-gray-100 active:text-red-500">{title}</h2>
+      <div className="flex px-10 sm:px-20 whitespace-nowrap space-x-10 sm:space-x-20 overflow-x-scroll text-2xl scrollbar-hide">
+        {Object.entries(requests).map(([key, { title, url }]) => (
+          <h2
+            onClick={() => router.push(`/?genere=${key}`)}
+            key={Math.random()}
+            className=" last:pr-24 cursor-pointer transition duration-100 transform hover:scale-125 hover:text-gray-100 active:text-red-500"
+          >
+            {title}
+          </h2>
         ))}
-        </div>
-        <div className="absolute top-0 right-0 bg-gradient-to-l from-[#06202A] h-10 w-1/12"/>
+      </div>
+      <div className="absolute top-0 right-0 bg-gradient-to-l from-[#06202A] h-10 w-1/12" />
     </nav>
-  )
-}
+  );
+};
 
-export default Nav
+Nav.displayName = "Nav";
+
+export default Nav;
